a27d41ec2ca35806a3d6d4d62e14aaeec68932ed
jarkkokauppinen <jarkkokauppinen77@gmail.com>
2022-09-30 15:15:37 +0300

    Test CORS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [movie, setMovie] = useState()
+
+  const getMovie = async () => {
+    const url = process.env.REACT_APP_BASE_URL
+    const movie = await fetch(`${url}/movie/1`)
+    setMovie(await movie.json())
+  }
+
+  console.log(movie)
+
+  useEffect(() => {
+    getMovie()
+  }, [])
+
   return (
     <div>
       mymovies
